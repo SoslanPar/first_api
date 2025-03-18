e0f77e081621d8e92dc5d339bcb7c0739020fafe
Soslan <soslanpar@yandex.ru>
2025-03-18 09:29:37 +0300

    add username and user_email

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 
 from fastapi import FastAPI, HTTPException
 import uvicorn
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
 
 api = FastAPI()
 
@@ -14,9 +14,13 @@ class Priority(IntEnum):
 
 
 class TodoBase(BaseModel):
-    todo_name: str = Field(..., min_lentgh=3, max_length=512, description='Name of the todo')
-    todo_description: str = Field(..., description='description of the todo')
+    todo_name: str = Field(..., min_lentgh=2, max_length=512, description='Name of the todo')
+    todo_description: str = Field(..., max_length=128, description='Description of the todo')
     priority: Priority = Field(default=Priority.LOW, description='Priority of the todo')
+    username: str = Field(..., min_length=2, max_length=64, description='Username')
+    user_email: EmailStr = Field(..., description='User email')
+
+    model_config = ConfigDict(extra='forbid')
 
 
 class TodoCreate(TodoBase):
@@ -31,28 +35,34 @@ class TodoUpdate(BaseModel):
     todo_name: Optional[str]= Field(None, min_lentgh=3, max_length=512, description='Name of the todo')
     todo_description: Optional[str] = Field(None, description='description of the todo')
     priority: Optional[Priority] = Field(None, description='Priority of the todo')
+    username: Optional[str] = Field(None, min_length=2, max_length=64, description='Username')
+    user_email: Optional[EmailStr] = Field(None, description='User email')
 
 
 
 all_todos = [
-    Todo(todo_id=1, todo_name='Sports', todo_description="Go to the gym", priority=Priority.MEDIUM),
-    Todo(todo_id=2, todo_name='Read', todo_description="Read 30 minutes", priority=Priority.MEDIUM),
-    Todo(todo_id=3, todo_name='History', todo_description="Edit document", priority=Priority.LOW),
-    Todo(todo_id=4, todo_name='Eat', todo_description="Eat with appetite and between 2000 and 2500 ccal", priority=Priority.MEDIUM),
-    Todo(todo_id=5, todo_name='Programming', todo_description="Practise programming", priority=Priority.HIGH),
+    Todo(todo_id=1, todo_name='Sports', todo_description="Go to the gym", priority=Priority.MEDIUM, username='Soslan', user_email='SP@yandex.ru'),
+    Todo(todo_id=2, todo_name='Read', todo_description="Read 30 minutes", priority=Priority.MEDIUM, username='Soslan', user_email='SP@yandex.ru'),
+    Todo(todo_id=3, todo_name='History', todo_description="Edit document", priority=Priority.LOW, username='Soslan', user_email='SP@yandex.ru'),
+    Todo(todo_id=4, todo_name='Eat', todo_description="Eat with appetite and between 2000 and 2500 ccal", priority=Priority.MEDIUM, username='Soslan', user_email='SP@yandex.ru'),
+    Todo(todo_id=5, todo_name='Programming', todo_description="Practise programming", priority=Priority.HIGH, username='Soslan', user_email='SP@yandex.ru'),
 ]
 
 
-@api.get('/todos', response_model=List[Todo])
-def get_todos(first_n: int = None):
+@api.get('/', tags = ['Hello'])
+def print_hello():
+    return 'Hello'
+
+@api.get('/todos', tags=['To do'])
+def get_todos(first_n: int = None) -> List[Todo]:
     if first_n:
         return all_todos[:first_n]
     else:
         return all_todos
     
 
-@api.get('/todos/{todo_id}', response_model=Todo)
-def get_todo(todo_id: int):
+@api.get('/todos/{todo_id}', tags=['To do'])
+def get_todo(todo_id: int) -> Todo:
     for todo in all_todos:
         if todo.todo_id == todo_id:
             return todo
@@ -60,37 +70,49 @@ def get_todo(todo_id: int):
     raise HTTPException(status_code=404, detail='Todo not found')
     
 
-@api.post('/todos', response_model=Todo)
-def create_todo(todo: TodoCreate):
+@api.post('/todos', tags=['To do'])
+def create_todo(todo: TodoCreate) -> Todo:
     new_todo_id = max(todo.todo_id for todo in all_todos) + 1
 
     new_todo = Todo(todo_id = new_todo_id,
                     todo_name = todo.todo_name,
                     todo_description = todo.todo_description,
-                    priority = todo.priority)
+                    priority = todo.priority,
+                    username = todo.username,
+                    user_email = todo.user_email)
 
     all_todos.append(new_todo)
 
     return new_todo
 
 
-@api.put('/todos/{todo_id}', response_model=Todo)
-def update_todo(todo_id: int, updated_todo: TodoUpdate):
+@api.put('/todos/{todo_id}', tags=['To do'])
+def update_todo(todo_id: int, updated_todo: TodoUpdate) -> Todo:
     for todo in all_todos:
         if todo.todo_id == todo_id:
+
             if updated_todo.todo_name is not None:
                 todo.todo_name = updated_todo.todo_name
+
             if updated_todo.todo_description is not None:
                 todo.todo_description = updated_todo.todo_description
+
             if updated_todo.priority is not None:
                 todo.priority = updated_todo.priority
+
+            if updated_todo.username is not None:
+                todo.username = updated_todo.username
+
+            if updated_todo.user_email is not None:
+                todo.user_email = updated_todo.user_email
+                
             return todo
         
     raise HTTPException(status_code=404, detail='Todo not found')
 
 
-@api.delete('/todos/{todo_id}', response_model=Todo)
-def delete_todo(todo_id: int):
+@api.delete('/todos/{todo_id}', tags=['To do'])
+def delete_todo(todo_id: int) -> Todo:
     for index, todo in enumerate(all_todos):
         if todo.todo_id == todo_id:
             delete_todo = all_todos.pop(index)
