fd1f088f6e9bd43573dde5321669883dfc39b14a
Soslan <soslanpar@yandex.ru>
2025-03-28 13:39:11 +0300

    add Dockerfile and I can use this api from docker_container

--- main.py
+++ main.py
@@ -202,4 +202,4 @@ async def get_streaming_file(filename: str):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:api', reload=True)
+    uvicorn.run('main:api', reload=True, port=8000, host="0.0.0.0")
